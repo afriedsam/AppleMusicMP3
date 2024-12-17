ba8d13b678686e86f4531f0b7d1d92064ade3e7e
Aidan Friedsam <afriedsam22@gmail.com>
2024-12-17 03:01:56 -0500

    Bump version to 0.1.1 and update README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AppleMusicMP3",
-    version="0.1.0",
+    version="0.1.1",
     author="Aidan Friedsam",
     description="Convert Apple Music songs to MP3.",
     long_description=open("README.md").read(),
